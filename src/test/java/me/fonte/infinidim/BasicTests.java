5f31f13fa68c5a2b7de0a2e37f3d73cbb31d9f58
Jeremy Fonte <jeremy@fonte.me>
2018-01-06 15:30:12 -0500

    Finished and fixed 3D InfiniDim simple test

--- src/test/java/me/fonte/infinidim/BasicTests.java
+++ src/test/java/me/fonte/infinidim/BasicTests.java
@@ -8,8 +8,8 @@ public class BasicTests {
 	public static void main(String[] args) {
 		System.out.println("Testing...");
 			
-		testBasic(5, 4, 1, 2);
-		testBasic(10, 4, 1, 2);
+		//testBasic(5, 4, 1, 2);
+		//testBasic(10, 4, 1, 2);
 		test3D(5, 10, 10, 1, 2, 2);
 	}
 	
@@ -30,8 +30,8 @@ public class BasicTests {
 		//set up the coords for the new value		
 		HashMap<Integer, Integer> coordHashMap = new HashMap<>();
 		
-		coordHashMap.put(0, xCoord); //x coord = 3rd (arrays start at 0)
-		coordHashMap.put(1, yCoord); //y coord = 2nd
+		coordHashMap.put(0, xCoord); //x coord
+		coordHashMap.put(1, yCoord); //y coord
 		
 		System.out.println("x Coordinate: " + xCoord);
 		System.out.println("y Coordinate: " + yCoord);
@@ -51,8 +51,28 @@ public class BasicTests {
 		
 		dimLenHashMap.put(0, xLen);
 		dimLenHashMap.put(1, yLen);
+		dimLenHashMap.put(2, zLen);
 		
 		System.out.println("x Length: " + xLen);
 		System.out.println("y Length: " + yLen);		
+		System.out.println("z Length: " + zLen);
+		
+		InfiniDim<Integer> testInf = new InfiniDim<>(3, dimLenHashMap);
+		
+		//set up the coords for the new value		
+		HashMap<Integer, Integer> coordHashMap = new HashMap<>();
+				
+		coordHashMap.put(0, xCoord); //x coord
+		coordHashMap.put(1, yCoord); //y coord
+		coordHashMap.put(2, zCoord); //z coord
+				
+		System.out.println("x Coordinate: " + xCoord);
+		System.out.println("y Coordinate: " + yCoord);
+		System.out.println("y Coordinate: " + zCoord);
+				
+		int result = testInf.getFlatIndex(coordHashMap);
+				
+				
+		System.out.println("Flattened Index Result: " + result);
 	}
 }
